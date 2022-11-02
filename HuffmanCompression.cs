8184ec6ac54d5144618af47e4ea2ec9ed28a6dbc
Martin Holub <80425277+MRmHolub@users.noreply.github.com>
2022-11-02 16:35:10 +0100

    Adding using system

--- HuffmanCompression.cs
+++ HuffmanCompression.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System;
 
 char[] s = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
